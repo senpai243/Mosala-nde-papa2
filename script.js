da1ae02fad23f88cd2aa1291d88e2dee0aaa39a0
senpai243 <sapiensbioshi@gmail.com>
2025-06-17 22:21:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function enterSite() {
+  document.getElementById("welcome-screen").style.display = "none";
+  document.getElementById("main-content").classList.remove("hidden");
+  window.scrollTo(0, 0);
+}
+
+console.log("Bienvenue sur Mosala Nde Papa - Site officiel.");
